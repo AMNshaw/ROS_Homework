f24a9b93c3fd8dc709e46dfdbd0845c75a33ea6b
AMNshaw <rosel94638@gmail.com>
2021-03-22 22:23:34 +0800

    0711044 HW3 completed.

--- hw3/src/eigen_transform.cpp
+++ hw3/src/eigen_transform.cpp
@@ -1,16 +1,19 @@
 #include "ros/ros.h"
 #include <Eigen/Dense>
 #include <cmath>
+const double PI = 3.14159265359;
 
 Eigen::Vector3d rad2deg(Eigen::Vector3d radians)
 {
     // Implement your code here
+    radians = radians * 180/PI;
     return radians;
 }
 
 Eigen::Vector3d deg2rad(Eigen::Vector3d degrees)
 {
     // Implement your code here
+    degrees = degrees * PI/180;
     return degrees;
 }
 
@@ -28,8 +31,32 @@ Eigen::Vector3d Quaternion2Euler(Eigen::Quaterniond Q)
     Eigen::Vector3d Euler(0, 0, 0);
     Euler.x() = atan2(2 * (Q.w() * Q.x() + Q.y() * Q.z()), (1 - 2 * (Q.x() * Q.x() + Q.y() * Q.y())));
     // Implement your code here
+    Euler.y() = asin(2 * (Q.w() * Q.y() + Q.z() * Q.x()));
+    Euler.z() = atan2(2 * (Q.x() * Q.y() + Q.w() * Q.z()), (1 - 2 * (Q.y() * Q.y() + Q.z() * Q.z())));
     return Euler;
 }
+
+Eigen::Quaterniond Hemilton(Eigen::Quaterniond q1, Eigen::Quaterniond q2)
+{
+    
+    double S1 = q1.w();
+    double S2 = q2.w();
+    Eigen::Vector3d V1 (q1.x(), q1.y(), q1.z());
+    Eigen::Vector3d V2 (q2.x(), q2.y(), q2.z());
+    Eigen::Vector3d Vpr;
+    Vpr = V2*S1 + V1*S2 + V1.cross(V2);
+    Eigen::Quaterniond product (S1*S2 - V1.dot(V2), Vpr.x(), Vpr.y(), Vpr.z());
+    return product;
+}
+
+Eigen::Quaterniond QuadVecRot(Eigen::Quaterniond p, Eigen::Quaterniond q)
+{
+    //p' = qpq^-1
+    Eigen::Quaterniond temp = Hemilton(p,q.conjugate());
+    Eigen::Quaterniond Pprime = Hemilton(q,temp);
+    return Pprime;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -41,20 +68,20 @@ int main(int argc, char **argv)
     Eigen::Vector3d tmp_point = world_point;
     Eigen::Vector3d euler_angle_deg(0, 0, 90);
     // porpotion of euler_angle
-    double s = 0.667;
+    double s = 0.66667;
     int count = 0;
     std::cout << "Current point position " << world_point.transpose() << std::endl << std::endl;
 
     while (ros::ok())
     {
         Eigen::Vector3d euler_angle_rad = deg2rad(euler_angle_deg);
-//        std::cout << euler_angle_rad.transpose() << std::endl;
+        std::cout << "euler_rad: " << euler_angle_rad.transpose() << std::endl;
 
         Eigen::Quaterniond Q_Total = Euler2Quaternion(euler_angle_rad);
-//        std::cout << Q_Total.coeffs().transpose() << std::endl;
+        std::cout << "Quaternion: " <<Q_Total.coeffs().transpose() << std::endl;
 
         Eigen::Quaterniond Q_tmp = Eigen::Quaterniond::Identity().slerp(s, Q_Total);
-//        std::cout << Q_tmp.coeffs().transpose() << std::endl;
+        std::cout << "Q_temp: " << Q_tmp.coeffs().transpose() << std::endl;
 
         Eigen::Vector3d euler_tmp_rad = Quaternion2Euler(Q_tmp);
         Eigen::Vector3d euler_tmp_deg = rad2deg(euler_tmp_rad);
@@ -63,7 +90,13 @@ int main(int argc, char **argv)
                   << ", yaw(Z): " << euler_tmp_deg.z() << std::endl;
 
         // Apply rotation to point with quaternion
+
         // Implement your code here
+        Eigen::Quaterniond Q_point(0, world_point.x(), world_point.y(), world_point.z());
+        Eigen::Quaterniond Pprime = QuadVecRot(Q_point, Q_tmp);
+        std::cout << "Self-Derived world point: " <<Pprime.coeffs().transpose() <<std::endl;
+
+        world_point = Q_tmp * world_point;
         count ++;
         std::cout << "Current point position " << world_point.transpose() << std::endl << std::endl;
 
